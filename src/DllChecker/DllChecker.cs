b6498c443fffa00c7f38cdcf02bc212a67816760
Nick Chelmis <nikos.helmis@gmail.com>
2020-01-29 21:50:25 +0200

    Add dll is release console

---
+++ src/DllChecker/DllChecker.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace DllChecker
+{
+    //https://www.hanselman.com/blog/HowToProgrammaticallyDetectIfAnAssemblyIsCompiledInDebugOrReleaseMode.aspx
+    public static class DllChecker
+    {
+        public static string ScanDirectory(string sDir)
+        {
+            var sb = new StringBuilder();
+            foreach (string file in Directory.GetFiles(sDir, "*.dll"))
+            {
+                InspectFile(file, sb);
+            }
+
+            var directories = Directory.GetDirectories(sDir);
+            if (directories.Length == 0) return sb.ToString();
+
+            foreach (string directory in directories)
+            {
+                if (directory.EndsWith("roslyn")) continue;
+                foreach (string file in Directory.GetFiles(directory, "*.dll"))
+                {
+                    InspectFile(file, sb);
+                }
+
+                ScanDirectory(directory);
+            }
+
+            return sb.ToString();
+        }
+
+        private static void InspectFile(string file, StringBuilder sb)
+        {
+            var assembly = Assembly.LoadFrom(file);
+            object[] attributes = GetDebuggableAttributes(assembly);
+            if (attributes == null) return;
+            if (attributes.Length == 0)
+            {
+                sb.AppendLine($"{file} is RELEASE build");
+                return;
+            }
+
+            foreach (DebuggableAttribute attr in attributes)
+            {
+                sb.AppendLine(attr.IsJITOptimizerDisabled
+                    ? $"{file} is DEBUG build. Run time optimizer is DISABLED. Run TimeTracking is {attr.IsJITTrackingEnabled}"
+                    : $"{file} is RELEASE build. Run time optimizer is ENABLED. Run TimeTracking is {attr.IsJITTrackingEnabled}");
+            }
+        }
+
+        private static object[] GetDebuggableAttributes(Assembly assembly)
+        {
+            try
+            {
+                return assembly.GetCustomAttributes(typeof(DebuggableAttribute), false);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/DllChecker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace DllChecker
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string directory = ConfigurationManager.AppSettings["directoryPath"] 
+                               ?? Directory.GetCurrentDirectory();
+            string result = DllChecker.ScanDirectory(directory);
+
+            using (var outputFile = new StreamWriter(
+                Path.Combine(Directory.GetCurrentDirectory(), "result.txt")))
+            {
+                outputFile.Write(result);
+            }
+        }
+    }
+}
+
